f4b1d599e882df10078dd7f4ac902ae29803984f
Erick CARON <erick.caron3@gmail.com>
2023-03-09 18:05:20 +0100

    creating infrastructure

---
+++ src/Structure.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.Optional;
+
+interface Structure {
+    // zwraca dowolny element o podanym kolorze
+    Optional<Block> findBlockByColor(String color);
+
+    // zwraca wszystkie elementy - blocki z danego materiału
+    List<Block> findBlocksByMaterial(String material);
+
+    //zwraca liczbę wszystkich elementów-blocki tworzących strukturę
+    int count();
+}
