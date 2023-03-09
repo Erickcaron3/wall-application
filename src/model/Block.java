611af4929b15e44e402cb4c0f8c3043ce43c7ebe
Erick CARON <erick.caron3@gmail.com>
2023-03-09 20:08:04 +0100

    count implementation

--- src/model/Block.java
+++ src/model/Block.java
@@ -14,16 +14,8 @@ public class Block {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public String getMaterial() {
         return material;
-    }
-
-    public void setMaterial(String material) {
-        this.material = material;
     }
 
     @Override
